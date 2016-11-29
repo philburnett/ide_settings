659dbf3283f5ccdf97f4460f9de8f432d7b9aea8
Phillip Burnett <phillip.burnett@worldfirst.com>
2016-11-29 15:21:41 +0000

    PS-2016.2.2 <phil@phil-Latitude-E5470 Update baseRefactoring.xml, PHP Class.php, ide.general.xml, Default.xml, project.default.xml, diff.xml, find.xml, ui.lnf.xml, web-browsers.xml, filetypes.xml, php.xml, PHP Interface.php, databaseSettings.xml, databaseDrivers.xml	Create keymap.xml, vcs.xml, other.xml, git.xml, laf.xml

--- fileTemplates/internal/PHP Class.php
+++ fileTemplates/internal/PHP Class.php
@@ -1,4 +1,6 @@
 <?php
+#parse("PHP File Header.php")
+
 #if (${NAMESPACE})
 
 namespace ${NAMESPACE};
@@ -7,11 +9,10 @@ namespace ${NAMESPACE};
 /**
  * Class ${NAME}
  *
- * @copyright Copyright (c) BBC (http://www.bbc.co.uk)
- * @author    BBC iBroadcast Developers <ibroadcast-dev@lists.forge.bbc.co.uk>
- *
+ * @author    Phillip Burnett <phillip.burnett@worldfirst.com>
+ * @copyright 2016 World First Ltd. All rights reserved.
+ * @version   1.0.0
  */
-class ${NAME}
-{
+class ${NAME} {
 
 }
--- fileTemplates/internal/PHP Interface.php
+++ fileTemplates/internal/PHP Interface.php
@@ -1,4 +1,6 @@
 <?php
+#parse("PHP File Header.php")
+
 #if (${NAMESPACE})
 
 namespace ${NAMESPACE};
@@ -7,9 +9,10 @@ namespace ${NAMESPACE};
 /**
  * Interface ${NAME}
  *
- * @author Phil Burnett <phil.burnett@valtech.co.uk>
+ * @author    Phillip Burnett <phillip.burnett@worldfirst.com>
+ * @copyright 2016 World First Ltd. All rights reserved.
+ * @version   1.0.0
  */
-interface ${NAME}
-{
+interface ${NAME} {
 
 }
